622d0f5d8813a96581ec7560f4f7987b71913dbd
rodmatola <rodmatola@gmail.com.com>
2017-09-23 09:23:56 -0300

    fist commit

---
+++ kata09/src/checkout/CheckOut.java
@@ -0,0 +1,52 @@
+package checkout;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CheckOut {
+	Map<String, Integer> Itens = new HashMap<String, Integer>();
+	{
+		Itens.put("A", 50);
+		Itens.put("B", 30);
+		Itens.put("C", 20);
+		Itens.put("D", 15);
+	}
+		
+	private int total;
+	private int valor;
+	
+	ArrayList<String> repetidos = new ArrayList<String>();
+	
+	public void scan(String item){
+		
+		int cont;
+		this.valor = Itens.get(item);
+		this.total += this.valor;
+		
+		repetidos.add(item);
+		cont =  Collections.frequency(repetidos, item);
+		
+		if (item == "A" && cont % 3 == 0){
+			this.total -= 20;			
+		}
+		
+		if (item == "B" && cont % 2 == 0){
+			this.total -= 15;
+		}		
+		
+	}
+	
+	public int total(){
+		return total;
+	}
+	
+	public void reset(){
+		this.valor = 0;
+		this.total = 0;
+	}
+	
+	
+	
+}
